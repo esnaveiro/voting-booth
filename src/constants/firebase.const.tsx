f924eb71bf6634e8198842dd0ba7c53d2c07f4f5
CondeP <pedro.conde@paddypowerbetfair.com>
2022-11-04 14:40:22 +0000

    first commit

---
+++ src/constants/firebase.const.tsx
@@ -0,0 +1,12 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyBISvmXzS4Y2hbAnQyF889lKjJut9iGxRo",
+    authDomain: "voting-booth-89633.firebaseapp.com",
+    projectId: "voting-booth-89633",
+    storageBucket: "voting-booth-89633.appspot.com",
+    messagingSenderId: "745370923612",
+    appId: "1:745370923612:web:acc6c040d8cdf077eb2c99"
+};
