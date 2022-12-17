4bc434b2642df2a2d62f1e8f4f94d7cd9bcb2ae2
ppconde <pepconde.1993@gmail.com>
2022-12-17 18:16:46 +0000

    added redirect to login page when accessing root

--- src/layouts/public.layout.tsx
+++ src/layouts/public.layout.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { Layout } from 'antd';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export const PublicLayout: React.FC = () => {
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const auth = getAuth();
+		onAuthStateChanged(auth, (user) => {
+			if (!user) {
+				// User is signed out
+				navigate('/login');
+			}
+		});
+
+	}, [navigate]);
+
 	return (
 		<Layout className="public-layout" style={{ minHeight: '100vh' }}>
 			<Outlet />
